c6e0758d9dfaa48df6e8b54c5f7c7023b99c9aaf
.Alex <alexbrex28@gmail.com>
2025-12-12 13:43:52 +0200

    Starting exercices 3.13-3.14

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,25 +1,10 @@
+require('dotenv').config
 const express = require('express')
 const app = express()
-
+const Note = require('./models/note')
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -37,18 +22,17 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note =>{
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  }) 
 })
 
 const generateId = () => {
@@ -61,21 +45,19 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
+
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
@@ -90,7 +72,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
